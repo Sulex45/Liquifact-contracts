31e743e42ef45bf52ab005d7ffeb23c4026d81d0
ZuLu0890 <ayodelepraise2000@gmail.com>
2026-06-18 22:06:40 +0000

    fix(escrow/tests): add missing max_per_investor and legal_hold_clear_delay args in init test calls

--- escrow/src/tests/init.rs
+++ escrow/src/tests/init.rs
@@ -350,6 +350,8 @@ fn test_init_invoice_id_non_ascii_multibyte_panics() {
         &None,
         &None,
         &None,
+        &None,
+        &None,
     );
 }
 
@@ -368,7 +370,20 @@ fn test_init_invoice_id_embedded_null_panics() {
     let s = soroban_sdk::String::from_bytes(&env, &bytes[..]);
 
     client.init(
-        &admin, &s, &sme, &1000i128, &500i64, &0u64, &t, &None, &tr, &None, &None, &None,
+        &admin,
+        &s,
+        &sme,
+        &1000i128,
+        &500i64,
+        &0u64,
+        &t,
+        &None,
+        &tr,
+        &None,
+        &None,
+        &None,
+        &None,
+        &None,
     );
 }
 
